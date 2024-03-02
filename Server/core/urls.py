05e41b7e89d30c48a76519753638c547e2ef0d89
Jake-Ephraim <ephraimakolo2017@gmail.com>
2024-03-02 17:35:50 +0100

    fixed swagger

--- Server/core/urls.py
+++ Server/core/urls.py
@@ -19,4 +19,9 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, Spec
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # Optional UI:
+    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]

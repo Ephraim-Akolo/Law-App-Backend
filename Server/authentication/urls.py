49417f14ac37cd56a84ae40f11a1ab0c6b6330e9
Jake-Ephraim <ephraimakolo2017@gmail.com>
2024-03-06 15:50:43 +0100

    login

--- Server/authentication/urls.py
+++ Server/authentication/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path, include
 # from rest_framework.authtoken.views import obtain_auth_token
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
 
 urlpatterns = [
     path('signup/', views.UserSignupView.as_view()),
+    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     # path('login/', obtain_auth_token),
 ]

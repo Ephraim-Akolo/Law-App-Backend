68a1981ba4fb2d6e671c66ae8bc253907a1fcadd
Jake-Ephraim <ephraimakolo2017@gmail.com>
2024-03-31 13:55:58 +0100

    added docker file

--- Server/build.sh
+++ Server/build.sh
@@ -4,9 +4,5 @@ set -o errexit  # exit on error
 
 pip install -r requirements.txt
 
-apt install libmagic1
-apt install tesseract-ocr
-apt install libtesseract-dev
-
 python manage.py collectstatic --no-input
 python manage.py migrate
